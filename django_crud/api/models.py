3eddbd153249c0a77973fa58dc3cab74d8178a95
Julfy11100100 <klimbog1@gmail.com>
2020-11-17 22:03:45 +0300

    add user model

--- django_crud/api/models.py
+++ django_crud/api/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    Модель юзеров для crud
+    """
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(
+        blank=False, max_length=150,
+        validators=[
+            RegexValidator(
+                regex='^[\\w.@+-]+$',
+                message='Некорректные входные данные username',
+                code='invalid_username'
+            ),
+        ]
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    password = models.CharField(
+        blank=False, max_length=128,
+        validators=[
+            RegexValidator(
+                regex='^(?=.*[A-Z])(?=.*\\d).{8,}$',
+                message='Некорректные входные данные password',
+                code='invalid_password'
+            ),
+        ]
+    )
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField()
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
