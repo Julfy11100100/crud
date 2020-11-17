e31df9b20cd900489fb93d7f3daf6b59bba18642
Julfy11100100 <klimbog1@gmail.com>
2020-11-18 00:29:06 +0300

    user serializers start

--- django_crud/api/models.py
+++ django_crud/api/models.py
@@ -7,30 +7,12 @@ class User(models.Model):
     Модель юзеров для crud
     """
     id = models.AutoField(primary_key=True)
-    username = models.CharField(
-        blank=False, max_length=150,
-        validators=[
-            RegexValidator(
-                regex='^[\\w.@+-]+$',
-                message='Некорректные входные данные username',
-                code='invalid_username'
-            ),
-        ]
-    )
+    username = models.CharField(blank=False, max_length=150)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    password = models.CharField(
-        blank=False, max_length=128,
-        validators=[
-            RegexValidator(
-                regex='^(?=.*[A-Z])(?=.*\\d).{8,}$',
-                message='Некорректные входные данные password',
-                code='invalid_password'
-            ),
-        ]
-    )
+    password = models.CharField(blank=False, max_length=128)
     is_active = models.BooleanField(default=True)
-    last_login = models.DateTimeField()
+    last_login = models.DateTimeField(auto_now_add=True, blank=True)
     is_superuser = models.BooleanField(default=False)
 
     def __str__(self):
